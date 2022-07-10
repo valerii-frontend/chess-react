a8ac6c371c206ccaaaf246e4594700edabb9d7ce
Valerii from earth <lvaldev.code@gmail.com>
2022-07-10 23:20:32 +0200

    add board and cell classes

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,4 +1,20 @@
-export enum Board {
-	WHITE = "white",
-	BlACK = "black",
+import { Cell } from "./Cell.1";
+import { Colors } from "./Colors";
+
+export class Board {
+	cells: Cell[][] = [];
+
+	public initCells() {
+		for (let i = 0; i < 8; i++) {
+			const row: Cell[] = [];
+			for (let j = 0; j < 8; j++) {
+				if ((i + j) % 2 !== 0) {
+					row.push(new Cell(this.cells, j, i, Colors.BlACK, null)); // black
+				} else {
+					row.push(new Cell(this.cells, j, i, Colors.WHITE, null)); // white
+				}
+				this.cells.push(row);
+			}
+		}
+	}
 }
--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -1,1 +1,22 @@
-export enum Cell {}
+import { Board } from "./Board";
+import { Colors } from "./Colors";
+import { Figure } from "./figures/Figure";
+
+export class Cell {
+	readonly x: number;
+	readonly y: number;
+	readonly color: Colors;
+	figure: Figure | null;
+	board: Board;
+	available: boolean; // show cells to move
+	id: number; // for react keys
+	constructor(board: Board, x: number, y: number, color: Colors, figure: Figure | null) {
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		this.figure = figure;
+		this.board = board;
+		this.available = false;
+		this.id = Math.random();
+	}
+}
