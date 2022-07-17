490f47b8a9d58e604313d43f5b2c6cc6857c2109
Valerii from earth <lvaldev.code@gmail.com>
2022-07-17 17:09:53 +0200

    add logic for pawn move

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -5,6 +5,8 @@ import blackLogo from "../../assets/black-pawn.svg";
 import whiteLogo from "../../assets/white-pawn.svg";
 
 export class Pawn extends Figure {
+	isFirstStep: boolean = true;
+
 	constructor(color: Colors, cell: Cell) {
 		super(color, cell);
 		this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
@@ -12,6 +14,22 @@ export class Pawn extends Figure {
 	}
 	canMove(target: Cell): boolean {
 		if (!super.canMove(target)) return false;
-		return true;
+
+		const direction = this.cell.figure?.color === Colors.BLACK ? 1 : -1;
+		const firstStepDirection = this.cell.figure?.color === Colors.BLACK ? 2 : -2;
+
+		if (
+			(target.y === this.cell.y + direction || (this.isFirstStep && target.y === this.cell.y + firstStepDirection)) &&
+			target.x === this.cell.x &&
+			this.cell.board.getCell(target.x, target.y).isEmpty()
+		) {
+			return true;
+		}
+		return false;
+	}
+
+	moveFigure(target: Cell) {
+		super.moveFigure(target);
+		this.isFirstStep = false;
 	}
 }
